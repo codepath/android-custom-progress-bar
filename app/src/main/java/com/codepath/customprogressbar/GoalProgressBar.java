5e4a9a17709d010bfb309870427c421cd7bdb6a7
Eric Petzel <epetzel@gmail.com>
2015-05-02 21:59:09 -0700

    initial commit

---
+++ app/src/main/java/com/codepath/customprogressbar/GoalProgressBar.java
@@ -0,0 +1,13 @@
+package com.codepath.customprogressbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ProgressBar;
+
+public class GoalProgressBar extends ProgressBar {
+
+    public GoalProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
---
+++ app/src/main/java/com/codepath/customprogressbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.codepath.customprogressbar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Random;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.progressBar) GoalProgressBar progressBar;
+
+    private final Random random = new Random();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        progressBar.setMax(100);
+        resetProgress();
+    }
+
+    @OnClick(R.id.resetProgressBtn)
+    public void resetProgress() {
+        int prog = random.nextInt(progressBar.getMax());
+        progressBar.setProgress(prog);
+    }
+}
