a3708a0709b6cec2a01675d603bd99d6977ef40e
Eric Petzel <epetzel@gmail.com>
2015-05-03 01:07:44 -0700

    do not resetProgress on rotation

--- app/src/main/java/com/codepath/customprogressbar/MainActivity.java
+++ app/src/main/java/com/codepath/customprogressbar/MainActivity.java
@@ -21,7 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
-        resetProgress();
+
+        if (savedInstanceState == null) {
+            resetProgress();
+        }
     }
 
     @OnClick(R.id.resetProgressBtn)
